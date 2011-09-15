c52acd83368c1f1248c51bbb969e76237ae0f0a8
Henrik Boennerup <bonnerup@h-bonnerup.dk>
2011-09-15 15:38:52 +0200

    Initial commit

---
+++ AbonnentSys/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbonnentSys::Application.config.secret_token = '65f7f60b2f4f2fedeb79003b324477bdd3a42c490ee4c844bd4525a55b42e79e7fc734718b8ba8d406cfc090d8bb2760b593b5446b3b3b991f2112135552791a'
